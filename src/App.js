5cb4f0825b5a19bbf13f905a2203603d990a10c6
woodystock <91628945+woodystock@users.noreply.github.com>
2021-11-29 14:43:34 +0000

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import CharacterContainer from './components/CharacterContainer';
 import Navigation from './components/Navigation';
+import axios from 'axios';
 
 function App() {
 
   // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
-
+  const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [characterFavourites, setCharacterFavourites] = useState([]);
+
+  const getCharacters = async (pageNumber) => {
+    // axios for api calls
+    const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+    setCharacters(apiResponse.data.data);
+  }
+
+  useEffect(() => {
+    console.log("useEffect triggers: " + currentPage)
+    getCharacters(currentPage);
+  }, [currentPage])
 
   return (
     <div className="page">
       <Header currentPage={currentPage} />
       <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer characters={characters} />
+      <CharacterContainer characters={characters}
+                          characterFavourites={characterFavourites}
+                          setCharacterFavourites={setCharacterFavourites} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,14 @@
 function Character({ character }) {
 
+  // default image
+  let imageSrc = "https://picsum.photos/id/237/200/300?blur";
+
+  //if the character has an image, then display it
+  if( character.imageUrl) {
+    // make sure to remove any extra params after the file...
+    imageSrc = character.imageUrl.substring( 0, character.imageUrl.indexOf('/revision'));
+  }
+
   return (
     <article className="character-item">
 
@@ -9,7 +18,7 @@ function Character({ character }) {
         Add to Favourites
       </div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSrc} alt={character.name} />
 
     </article>
   )
